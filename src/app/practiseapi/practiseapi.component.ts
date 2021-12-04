c1d597ec4752a621fbc999837b613c4a648ce310
unknown <swati26081997@gmail.com>
2021-12-04 20:40:22 +0530

    changes in branch-1

--- src/app/practiseapi/practiseapi.component.ts
+++ src/app/practiseapi/practiseapi.component.ts
@@ -1,21 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-practiseapi',
   templateUrl: './practiseapi.component.html',
   styleUrls: ['./practiseapi.component.css']
 })
 export class PractiseapiComponent implements OnInit {
-  url="https://jsonplaceholder.typicode.com/todos";
-   apidata:any;
+  //url="https://jsonplaceholder.typicode.com/todos";
+  // apidata:any={};
+   apidata: any ;
 
-   constructor(public http:HttpClient) {}
+   constructor(public http:HttpClient ) {
+      //this.apidata= employeeData;
+    }
 
    ngOnInit() {
-     this.http.get(this.url).subscribe(data=>{
+     this.http.get('assets/employeeData.json').subscribe(data=>{
       this.apidata= data;
+      console.log(this.apidata);
      });
-  }
+    }
 
 }
